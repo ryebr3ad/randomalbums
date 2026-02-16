b74c94ac3a803c423fb27eb48727b74d25fee547
= <rprauschenberg@gmail.com>
2026-02-16 00:55:23 -0500

    Initial commit

---
+++ RandomAlbums.java
@@ -0,0 +1,84 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+
+public class RandomAlbums {
+
+    public static void main(String args[]) {
+        int totalAlbums = 5; // 10 is a good default IMO
+        if (args.length >= 1) {
+            try {
+                totalAlbums = Integer.parseInt(args[0]);
+            } catch (NumberFormatException nfe) {
+                System.err.println(args[0] + " is not a number");
+                System.exit(-1);
+            }
+        }
+        HttpClient client = HttpClient.newHttpClient();
+
+        sendRequest(client, "http://localhost:8880/api/playlists/p3/clear", "");
+
+        Path startPath = Paths.get("C:\\Users\\rprau\\Music");
+        try (Stream<Path> stream = Files.walk(startPath)) {
+            List<Path> albums = stream.filter(p -> p.toString().split("\\\\").length == 6).toList();
+            List<Path> tracks = new ArrayList<>();
+            int size = albums.size();
+
+            for (int i = 0; i < totalAlbums; i++) {
+                Stream<Path> streamAgain = Files.walk(startPath);
+                Path pickedAlbum = albums.get(ThreadLocalRandom.current().nextInt(size));
+                List<Path> pickedTracks = streamAgain
+                        .filter(p -> p.startsWith(pickedAlbum) && p.toString().split("\\\\").length > 6).toList();
+                tracks.addAll(pickedTracks);
+                streamAgain.close();
+            }
+
+            sendRequest(client, "http://localhost:8880/api/playlists/p3/items/add", buildBody(tracks));
+            sendRequest(client, "http://localhost:8880/api/player/play/p3/0", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Body modeled for Beefweb API v. 0.11
+    private static String buildBody(List<Path> tracks) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('{');
+        sb.append("\"items\": [");
+        for (int i = 0; i < tracks.size(); i++) {
+            var track = tracks.get(i);
+            String trackStr = track.toString();
+            String formattedTrackStr = trackStr.replace("\\", "/");
+            sb.append("\"" + formattedTrackStr + "\"");
+            if (i < tracks.size() - 1) {
+                sb.append(',');
+            }
+        }
+        sb.append("]");
+        sb.append('}');
+        return sb.toString();
+    }
+
+    private static void sendRequest(HttpClient client, String path, String body) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(path))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+        try {
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
